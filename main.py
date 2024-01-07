74e92061394ed7c6bf8ed97824dc82981109916a
IlyaNovosad <IlyaNovosad@mail.ru>
2024-01-08 02:23:44 +0300

    First step

---
+++ main.py
@@ -0,0 +1,19 @@
+# Открыть файл
+# сохранить файл
+# Создание контакта
+# изм контакт
+# Найти контакт
+# Удалить контакт
+# Показать контакт
+# Выход
+import file_opener_reader_writer as frw
+count_id = 0
+def print_menu():
+    print("1. Create contact")
+    print("2. Change contact")
+    print("3. Find contact")
+    print("4. Delete contact")
+    print("5. Show contacts")
+    print("6. Exit")
+    print("7. Copy line in another file")
+    print("8. Save changes")
